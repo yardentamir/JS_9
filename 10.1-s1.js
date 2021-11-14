1aa6d55f7bc1abd383c31fd7b8b07924b1d20446
yarentamir <yarden.tamir@walla.co.il>
2021-11-14 14:47:03 +0200

    10.1 two solutions

--- 10.1-s1.js
+++ 10.1-s1.js
@@ -1,9 +1,8 @@
 (function () {
   "use strict";
-
   const isString = (str, func) => {
     if (typeof str === "string") {
-      func(str);
+      func(str, addDashes);
     }
   };
 
@@ -17,20 +16,19 @@
     for (const word of wordArr) {
       newArr.push(word[0].toUpperCase() + word.slice(1));
     }
-    func(newArr.join(" "));
+    func(newArr.join(" "), addDot);
   };
 
-  const addDashes = (str) => {
+  const addDashes = (str, func) => {
     let arrStr = str.split(" ");
-    console.log(arrStr.join("/"));
+    func(arrStr.join("/"), logConsole);
   };
 
   const addDot = (str, func) => {
     func((str += "."));
   };
 
-  isString("hello my name is yarden", logConsole);
-  firstWordUpperCase("how are you?", addDashes);
-  firstWordUpperCase("hi there", logConsole);
-  addDot("hello my name is yarden", addDashes);
+  isString("hello my name is yarden", firstWordUpperCase);
+  addDashes("hello my name is yarden", addDot);
+  addDot("hello my name is yarden", logConsole);
 })();
