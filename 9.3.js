b5ec762cd64b3e5463c686ccfe1f236a846b1f5f
yarentamir <yarden.tamir@walla.co.il>
2021-11-14 11:36:08 +0200

    9.3

--- 9.3.js
+++ 9.3.js
@@ -1,3 +1,20 @@
 (function () {
   "use strict";
+
+  const food = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber", "Olives"];
+  const food1 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
+
+  const foodFunc = (arr, arr1) => {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr1.length; j++) {
+        if (arr[i] === arr1[j]) {
+          newArr.push(arr[i]);
+        }
+      }
+    }
+    return newArr;
+  };
+
+  console.log(foodFunc(food, food1));
 })();
