8dcbd496fac0d024800355e89aa5dd9a0e5c6a22
yarentamir <yarden.tamir@walla.co.il>
2021-11-14 14:10:42 +0200

    10.1

---
+++ 10.1.js
@@ -0,0 +1,36 @@
+(function () {
+  "use strict";
+
+  const isString = (str, func) => {
+    if (typeof str === "string") {
+      func(str);
+    }
+  };
+
+  const logConsole = (str) => {
+    console.log(str);
+  };
+
+  const firstWordUpperCase = (str, func) => {
+    const wordArr = str.split(" ");
+    const newArr = [];
+    for (const word of wordArr) {
+      newArr.push(word[0].toUpperCase() + word.slice(1));
+    }
+    func(newArr.join(" "));
+  };
+
+  const addDashes = (str) => {
+    let arrStr = str.split(" ");
+    console.log(arrStr.join("/"));
+  };
+
+  const addDot = (str, func) => {
+    func((str += "."));
+  };
+
+  isString("hello my name is yarden", logConsole);
+  firstWordUpperCase("how are you?", addDashes);
+  firstWordUpperCase("hi there", logConsole);
+  addDot("hello my name is yarden", addDashes);
+})();
--- 9.4.js
+++ 9.4.js
@@ -1,23 +1,36 @@
 (function () {
   "use strict";
 
+  // Without the spaces it also looks good but you asked for it
+
   // solution 1 - with repeat
   const steps = (N) => {
     for (let i = 1; i <= N; i++) {
-      console.log("#".repeat(i));
+      console.log("#".repeat(i) + " ".repeat(N - i));
     }
   };
 
   // Ninja solution - without repeat
   const steps2 = (N) => {
-    let row = "";
     for (let i = 1; i <= N; i++) {
+      let row = "";
       for (let j = 1; j <= i; j++) row += "#";
+      for (let z = N; z >= i; z--) row += " ";
       console.log(row);
-      row = "";
+    }
+  };
+
+  const steps3 = (N) => {
+    for (let i = 1; i <= N; i++) {
+      let row = "";
+      for (let j = 1; j <= N; j++) {
+        row += j <= i ? "#" : " ";
+      }
+      console.log(row);
     }
   };
 
   steps(4);
   steps2(4);
+  steps3(4);
 })();
