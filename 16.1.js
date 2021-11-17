c454b0ec22aba267db957faafc213e4e8cb0ff8f
yarentamir <yarden.tamir@walla.co.il>
2021-11-17 14:45:11 +0200

    17.1

--- 16.1.js
+++ 16.1.js
@@ -18,5 +18,5 @@ If there are any faulty outputs, please write on how we can fix them.
 // ! answer
 /* 
 in line 2: will be "undefined" because the variable a declared afterwards
-in line 3: will be "2" because no matter where you call the function it will run because it's on the same scope
+in line 3: will be "2" because no matter where you call the function it will run because it's on the same scope or global function
 */
