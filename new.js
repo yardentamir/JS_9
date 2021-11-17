a3fd3ad568783eab149ec2038d96e628aee54368
yarentamir <yarden.tamir@walla.co.il>
2021-11-17 22:41:14 +0200

    b

--- new.js
+++ new.js
@@ -32,20 +32,55 @@
 
 // console.log(expressionMatter(6, 7, 1));
 
-function elevatorDistance(array) {
-  return array.reduce((perv, cur, i) => {
-    if (array.length - 2 == i) array.splice(i);
-    return Math.max(perv, cur) - Math.min(perv, cur);
-  });
+// function elevatorDistance(array) {
+//   return array.reduce((perv, cur, i) => {
+//     if (array.length - 2 == i) array.splice(i);
+//     return Math.max(perv, cur) - Math.min(perv, cur);
+//   });
+// }
+
+// function elevatorDistance2(array) {
+//   const newArr = array.map((value, i) => {
+//     return Math.max(value, array[i + 1]) - Math.min(value, array[i + 1]);
+//   });
+//   return newArr
+//     .splice(0, newArr.length - 1)
+//     .reduce((total, cur) => total + cur);
+// }
+
+// console.log(elevatorDistance2([7, 1, 7, 1]));
+
+// // complete the function
+// function solution(string) {
+//   let newStr = "";
+//   for (let i = 0; i < string.length; i++) {
+//     let char = string[i];
+//     if (char === char.toUpperCase()) {
+//       char = " " + char;
+//     }
+//     newStr += char;
+//   }
+//   return newStr;
+// }
+
+// console.log(solution("helloWorldCaseWhatsUp"));
+
+// function alphabetPosition(text) {
+//   let result = "";
+//   for (let i = 0; i < text.length; i++) {
+//     let code = text.toUpperCase().charCodeAt(i);
+//     if (code > 64 && code < 91) result += code - 64 + " ";
+//   }
+
+//   return result.slice(0, result.length - 1);
+// }
+// console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+
+function revrot(str, sz) {
+  if (str === "" && sz === 0) return "";
+  const strArr = str.split("");
+  const first = strArr[0].splice(1);
+  console.log(first);
 }
 
-function elevatorDistance2(array) {
-  const newArr = array.map((value, i) => {
-    return Math.max(value, array[i + 1]) - Math.min(value, array[i + 1]);
-  });
-  return newArr
-    .splice(0, newArr.length - 1)
-    .reduce((total, cur) => total + cur);
-}
-
-console.log(elevatorDistance2([7, 1, 7, 1]));
+console.log(revrot("123456987654", 6));
