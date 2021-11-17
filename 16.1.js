041bfde71f1a734e1e6b8f9a0607ed8330c73d22
yarentamir <yarden.tamir@walla.co.il>
2021-11-17 14:02:26 +0200

    16.1

---
+++ 16.1.js
@@ -0,0 +1,22 @@
+function funcA() {
+  console.log(a);
+  console.log(foo());
+  var a = 1;
+  function foo() {
+    return 2;
+  }
+}
+funcA();
+
+// ! question
+/*
+Without running the code below, explain in your own words what the result
+of each block of code will be and why.
+If there are any faulty outputs, please write on how we can fix them.
+*/
+
+// ! answer
+/* 
+in line 2: will be "undefined" because the variable a declared afterwards
+in line 3: will be "2" because no matter where you call the function it will run because it's on the same scope
+*/
